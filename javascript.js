15cc17dc8fd0ee4937d48826b7c400cc4e7b80c5
harsh baliyan <harshbaliyan21@gmail.com>
2025-12-09 09:03:27 +0530

    queryselector

--- javascript.js
+++ javascript.js
@@ -1,6 +1,19 @@
-const item = document.getElementsByTagName('li');
-item[4].style.color = "red";
-for (let i = 0; i < item.length; i++)
+// Write the code as shown in the video below:
+
+// Write answer to the questions asked below:
+const item = document.querySelector('#main-heading');
+item.style.textAlign = 'right';
+document.querySelector('#basket-heading').style.color = 'brown';
+const item1 = document.querySelectorAll('.fruit');
+for (let i = 0; i < item1.length; i++)
 {
-    item[i].style.fontStyle = "italic";
+    if (i % 2 != 0)
+    {
+        item1[i].style.backgroundColor = 'brown';
+        item1[i].style.color = 'white';
+    }
+    else
+        item1[i].style.backgroundColor = 'white';
+    
 }
+document.querySelector('.fruits').style.listStyle= 'none';
