1954d2d71bb718cc7941b2043440d687af0e4183
harsh baliyan <harshbaliyan21@gmail.com>
2025-12-26 17:33:12 +0530

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,89 @@
-// Write your code below:
-const para = document.createElement('h3');
-const paratext = document.createTextNode('Buy high quality organic fruits online');
-para.appendChild(paratext);
+let editLi = null;
 
-const divs = document.getElementsByTagName('div');
-divs[0].appendChild(para);
+window.onload = function () {
+    let savedData = localStorage.getItem("expenses");
+    if (savedData) {
+        document.getElementById("showDetail").innerHTML = savedData;
+        attachEvents();
+    }
+};
 
-const para2 = document.createElement('p');
-const paratext2 = document.createTextNode('Total fruits: 4');
-const item = document.querySelector('.fruits');
-para2.appendChild(paratext2);
-divs[1].insertBefore(para2, item);
+function addExpense() {
+    let expenseAmount = document.getElementById("expenseamount").value;
+    let description = document.getElementById("discription").value;
+    let chosecatagory = document.getElementById("category").value;
 
-para2.id = 'fruits-total';
+    // UPDATE MODE
+    if (editLi !== null) {
+        editLi.firstChild.nodeValue =
+            "Expense Amount: " + expenseAmount +
+            " | Description: " + description +
+            " | Category: " + chosecatagory + " ";
 
-para.style.fontStyle = 'italic';
+        editLi = null;
+    }
+    // ADD MODE
+    else {
+        let li = document.createElement("li");
 
+        li.appendChild(document.createTextNode(
+            "Expense Amount: " + expenseAmount +
+            " | Description: " + description +
+            " | Category: " + chosecatagory + " "
+        ));
 
+        // DELETE BUTTON
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Delete Expense";
 
+        deleteBtn.onclick = function () {
+            li.remove();
+        };
+
+        // EDIT BUTTON
+        let editBtn = document.createElement("button");
+        editBtn.innerHTML = "Edit Expense";
+
+        editBtn.onclick = function () {
+            document.getElementById("expenseamount").value = expenseAmount;
+            document.getElementById("discription").value = description;
+            document.getElementById("category").value = chosecatagory;
+            editLi = li;
+        };
+
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
+
+        document.getElementById("showDetail").appendChild(li);
+    }
+	saveToLocalStorage()
+    clearInputs();
+	attachEvents
+    
+}
+function attachEvents() {
+    let listItems = document.getElementById("showDetail").getElementsByTagName("li");
+    for (let i = 0; i < listItems.length; i++) {
+        let deleteBtn = listItems[i].getElementsByTagName("button")[0];
+        let editBtn = listItems[i].getElementsByTagName("button")[1];
+        deleteBtn.onclick = function () {
+            listItems[i].remove();
+            saveToLocalStorage();
+        };
+        editBtn.onclick = function () {
+            document.getElementById("expenseamount").value = listItems[i].firstChild.nodeValue.split("Expense Amount: ")[1].split(" | Description: ")[0];
+            document.getElementById("discription").value = listItems[i].firstChild.nodeValue.split(" | Description: ")[1].split(" | Category: ")[0];
+            document.getElementById("category").value = listItems[i].firstChild.nodeValue.split(" | Category: ")[1];
+            editLi = listItems[i];
+        };
+    }
+}
+function saveToLocalStorage() {
+    localStorage.setItem("expenses", document.getElementById("showDetail").innerHTML);
+}
+
+function clearInputs() {
+    document.getElementById("expenseamount").value = "";
+    document.getElementById("discription").value = "";
+    document.getElementById("category").value = "";
+}
